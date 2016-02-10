8b8285d807e61c6a0eed7d6f81b2770c159538d9
Christian <borachristia@gmail.com>
2016-02-10 16:00:46 -0600

    debugging

--- recastrestapi/apiconfig.py
+++ recastrestapi/apiconfig.py
@@ -9,7 +9,7 @@ def mk_config():
     the_config = default_config()
     if os.environ.has_key('RECASTCONTROLCENTER_CONFIG'):
         custom_config = yaml.load(open(os.environ['RECASTCONTROLCENTER_CONFIG']))
-        the_config_update(**custom_config)
+        the_config.update(**custom_config)
     return the_config
 
 config = mk_config()
