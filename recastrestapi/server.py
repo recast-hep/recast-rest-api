df85a37a63745f67a62b459f14d91a454c066c6b
Christian <borachristia@gmail.com>
2016-02-10 17:52:51 -0600

    adding settings file

--- recastrestapi/server.py
+++ recastrestapi/server.py
@@ -2,24 +2,29 @@ import recastdb.models
 from recastdb.database import db
 
 from eve import Eve
+from eve.auth import TokenAuth
 from eve_sqlalchemy import SQL
 from eve_sqlalchemy.validation import ValidatorSQL
 from eve_sqlalchemy.decorators import registerSchema
 
 from recastrestapi.apiconfig import config as apiconf
+from settings import DOMAIN, SQLALCHEMY_DATABASE_URI, DEBUG, XML, JSON
 
-registerSchema('users')(recastdb.models.User)
-registerSchema('analysis')(recastdb.models.Analysis)
-registerSchema('subscriptions')(recastdb.models.Subscription)
+
+class TokenAuth(TokenAuth):
+    def check_auth(self, token, allowed_roles, resource, method):
+        """ 
+            Token based authentications 
+        """
+        #accounts = app.data.driver.db['accounts']
+        return accounts.find_one({'token': token})
 
 SETTINGS = {
-    'DEBUG': True,
-    'SQLALCHEMY_DATABASE_URI': apiconf['DBPATH'],
-    'DOMAIN': {
-        'users': recastdb.models.User._eve_schema['users'],
-        'analysis': recastdb.models.Analysis._eve_schema['analysis'],
-        'subscriptions': recastdb.models.Subscription._eve_schema['subscriptions'],
-        }
+    'DOMAIN': DOMAIN,
+    'SQLALCHEMY_DATABASE_URI': SQLALCHEMY_DATABASE_URI,
+    'DEBUG': DEBUG,
+    'XML': XML,
+    'JSON': JSON,
 }
 
 app = Eve(auth=None, settings=SETTINGS, validator=ValidatorSQL, data=SQL)
